4617f5c57e9052b79a4a4d5e93d1498ea593acc7
rajat mathur <rajat1998mathur@gmail.com>
2019-04-13 10:53:10 +0530

    component part1

---
+++ src/app/shared/dish.ts
@@ -0,0 +1,12 @@
+//class created for dish of menu card jisko import kiya menu.component.ts mai
+
+export class Dish {
+    id: string;
+    name: string;
+    image: string;
+    category: string;
+    featured: boolean;
+    label: string;
+    price: string;
+    description: string;
+}
