d0855167bddb235e336134b0c8c5fcfe33acc0ca
rajat mathur <rajat1998mathur@gmail.com>
2019-06-25 14:04:07 +0530

    rxjs_part2

--- src/app/services/dish.service.ts
+++ src/app/services/dish.service.ts
@@ -30,9 +30,13 @@ getFeaturedDish(): Observable<Dish>
   return of(DISHES.filter((dish) =>dish.featured)[0]).pipe(delay(2000));
   }
   
+
+
+getDishIds(): Observable<string[] | any> {
+  return of (DISHES.map(dish =>dish.id));
 }
 
-
+}
 
   //created a method that return dishes mtlb dishes ki sari value getdishes may aa jayengi
  //1promise with delay
